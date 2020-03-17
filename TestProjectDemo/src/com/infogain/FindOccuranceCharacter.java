5cd7675d3fbb079b4f39d6960213ccd9d3bd98de
Md.Rahman <Md.Rahman@MdRahman.igglobal.com>
2020-03-17 16:58:02 +0530

    add some comment by haseeb

--- TestProjectDemo/src/com/infogain/FindOccuranceCharacter.java
+++ TestProjectDemo/src/com/infogain/FindOccuranceCharacter.java
@@ -5,15 +5,16 @@ import java.util.Map;
 
 public class FindOccuranceCharacter {
 	public static void main(String[] args) {
-		String str="haseeb";
-		char ch[]=str.toCharArray();
-		Map<Character,Integer>map=new HashMap<Character,Integer>();
-		for(char c:ch) {
-			if(!map.containsKey(c)) {
-				map.put(c,1);
-			}else {
-				int cnt=map.get(c);
-				map.put(c, cnt+1);
+		/* finding the string repeated character */
+		String str = "haseeb";
+		char ch[] = str.toCharArray();
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : ch) {
+			if (!map.containsKey(c)) {
+				map.put(c, 1);
+			} else {
+				int cnt = map.get(c);
+				map.put(c, cnt + 1);
 			}
 			System.out.println(map);
 		}
